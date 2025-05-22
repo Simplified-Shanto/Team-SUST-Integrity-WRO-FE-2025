c17d5919d4c48cfb7cf5a121ae1032c689ca2a09
Nayeem Islam Shanto <117001505+Simplified-Shanto@users.noreply.github.com>
2025-05-22 20:02:38 +0600

    Added local arduino codebase

---
+++ WRO Future engineers/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include "sonar.h"
+#include "motors.h"
+#include "servo_motor.h"
+
+
+
+
+void setup() {
+  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+  steering_servo.attach(steering_servo_pin);
+  pinMode(pwmPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  steering_servo.write(midAngle);
+  goForward(150); 
+}
+
+double error = 0;
+
+double maxError = 60; // Considering that the maximum difference
+                    //  of reading of the two sensors is 20 cm in the tunnel
+void loop() {
+  if(leftSonar.ping_cm() >= rightSonar.ping_cm())
+  {
+   error = leftSonar.ping_cm() - rightSonar.ping_cm(); 
+
+  }
+  else
+  {
+    error = rightSonar.ping_cm() - leftSonar.ping_cm(); 
+  }
+  byte steer_angle = (error / maxError) * halfAngleRange;
+
+
+
+  // Proportional response to the error. 
+  
+  if(leftSonar.ping_cm() >= rightSonar.ping_cm() and maxError > error)
+  {
+    steering_servo.write(midAngle + steer_angle);
+    
+  }
+  else if(leftSonar.ping_cm() < rightSonar.ping_cm() and maxError > error)
+  {
+     
+    steering_servo.write(midAngle - steer_angle); 
+  }
+
+  Serial.print("Left = ");
+  Serial.print(leftSonar.ping_cm());
+  Serial.print(" Right = ");
+  Serial.print(rightSonar.ping_cm()); 
+  Serial.print(" Error = ");
+  Serial.print(error);
+  Serial.print(" Steer = ");
+  Serial.print(steer_angle); 
+  Serial.println(); 
+}
---
+++ WRO Future engineers/src/servo_motor.h
@@ -0,0 +1,8 @@
+#include <Servo.h> 
+Servo steering_servo;
+
+#define steering_servo_pin 9
+#define leftAngle 20
+#define midAngle 70
+#define rightAngle 120
+double halfAngleRange = 50;
