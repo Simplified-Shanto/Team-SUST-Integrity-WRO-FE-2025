65c091c6e7ad706d850b74bced7f2e1ce5d2493f
Nayeem Islam Shanto <117001505+Simplified-Shanto@users.noreply.github.com>
2025-05-22 20:52:34 +0600

    Added arduino compatible

---
+++ arduino nano code/main/sonar_sensor.h
@@ -0,0 +1,10 @@
+#include <NewPing.h>
+///////////////////////////////////////////////////////////////////////// ---------------------------------------------------------------------------
+// Example NewPing library sketch that pings 3 sensors 20 times a second.
+// ---------------------------------------------------------------------------
+
+#define MAX_DISTANCE 100 // Maximum distance (in cm) to ping.
+
+NewPing middleSonar = NewPing(10, 10, MAX_DISTANCE);
+NewPing leftSonar = NewPing(2, 2, MAX_DISTANCE);
+NewPing rightSonar = NewPing(3, 3, MAX_DISTANCE); 
