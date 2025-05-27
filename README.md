d4a25adaece5ff2b5b7ffc3d16baf4082707970d
Nayeem Islam Shanto <islamnayeem386@gmail.com>
2025-05-27 10:36:34 +0600

    PD_Start_Stop
    1. Added derivative in PID
    2. Added a Learnings text file for documenting what works and what doesn't.
    3. Added vehicle starting and  stopping feature via serial command

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WRO-Future-Engineers-2025-
 This repository will contain the resources we develop for participating in the Future Engineers Category of WRO 2025
+
+
+Our experience with the TT motor says that, it's not perfect for the purpose because, it gets hot as the practice run keeps lengthening. 
+-> There's problem in the startup, motor starts slower than expected and after few seconds, it gains the proper speed. This is a problem. It might be a motor driver issue too. 
--- esp32codes/esp32codes.ino
+++ esp32codes/esp32codes.ino
@@ -1,20 +1,22 @@
-#include <Preferences.h> 
+#include <Preferences.h>
 #include "motors.h"
 #include "button.h"
 #include "servo_motor.h"
 #include "sonar.h"
 #include "display.h"
 
-Preferences preferences; 
- double Kp = 0; 
+Preferences preferences;
+double Kp = 0;
+double Kd = 0;
 //int forwardSpeed = 120 // Out of 255
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  preferences.begin("wrobot", false); 
- Kp =  preferences.getDouble("Kp", 0);
- forwardSpeed = preferences.getInt("speed", 0); 
+  preferences.begin("wrobot", false);
+  Kp = preferences.getDouble("Kp", 0);
+  Kd = preferences.getDouble("Kd", 0);
+  forwardSpeed = preferences.getInt("speed", 0);
 
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
@@ -38,111 +40,103 @@ void setup() {
 double value = 0;     // Stores the difference between the left and right readings.
 double setpoint = 0;  //The amount of difference in reading of the two ultrasonic sensor we want.
 double error = 0;     // The difference between value and setpoint.
-
+double lastError = 0;
 
 int setPoint = 0;  //Setpoint for the difference between the readings of the left and right sonar.
 
-//Serial.println("Kp = "); 
+//Serial.println("Kp = ");
 
 
 void loop() {
-    if (Serial.available()) {
-    String command = Serial.readStringUntil(';');  
-    char constant_name = command[0]; //The type of action the remote wants us to take. 
-    String constant_value_string = command.substring(2, command.length()); 
+  if (Serial.available()) {
+    String command = Serial.readStringUntil(';');
+
+    char constant_name = command[0];  //The type of action the remote wants us to take.
+    String constant_value_string = command.substring(2, command.length());
     // Serial.print("constant_value_string = ");
-    // Serial.println(constant_value_string);  
+    // Serial.println(constant_value_string);
     float constant_value = constant_value_string.toFloat();
     // Serial.print("constant value = ");
-    // Serial.println(constant_value); //Though there are more decimal places in the variable, 
+    // Serial.println(constant_value); //Though there are more decimal places in the variable,
     //                                 // the print function only prints upto two decimal places
-    switch(constant_name)
-    {
-      case 'p':
-        Kp = constant_value; 
-        preferences.putDouble("Kp", Kp); 
-        preferences.end(); 
-        break; 
-      case 's':
-        forwardSpeed = int(constant_value); 
-        preferences.putInt("speed", forwardSpeed); 
-        preferences.end(); 
-        break; 
-      default: 
-      break; 
-    }                     
+    if (command == "x") {  // Commands the car to stop
+      if (gameStarted) {
+        gameStarted = 0;
+        goForward(0);
+        steering_servo.write(midAngle);
+      }
+      else
+      {
+        gameStarted = 1; 
+        goForward(forwardSpeed); 
+      }
+    } else {
+      switch (constant_name) {
+        case 'p':  //Proportional of PID
+          Kp = constant_value;
+          preferences.putDouble("Kp", Kp);
+          break;
+        case 'd':  //Derivative of PID
+          Kd = constant_value;
+          preferences.putDouble("Kd", Kd);
+          break;
+        case 's':  //Speed of vehicle
+          forwardSpeed = int(constant_value);
+          preferences.putInt("speed", forwardSpeed);
+          break;
+        default:
+          break;
+      }
+      preferences.end();  // Saves variables to EEPROM
     }
+  }
   checkButton();
 
   int frontDistance = middleSonar.ping_cm();
-    int leftDistance = leftSonar.ping_cm();
-    int rightDistance = rightSonar.ping_cm();
+  int leftDistance = leftSonar.ping_cm();
+  int rightDistance = rightSonar.ping_cm();
 
-    if (leftDistance==0) { leftDistance = 100; }
-    else if (rightDistance==0){ rightDistance = 100; }
+  if (leftDistance == 0) {
+    leftDistance = 100;
+  } else if (rightDistance == 0) {
+    rightDistance = 100;
+  }
 
-    value = leftDistance - rightDistance;
-    error = value - setpoint;
-   // int frontConstant = 40;
+  value = leftDistance - rightDistance;
+  error = value - setpoint;
+  // int frontConstant = 40;
   //  double frontProportion = 1;
-   // double multiplier = (frontConstant - frontDistance) * (frontDistance < 20) * frontProportion;
- //   if (multiplier == 0) { multiplier = 1; }
-    double angle = error * Kp;
+  // double multiplier = (frontConstant - frontDistance) * (frontDistance < 20) * frontProportion;
+  //   if (multiplier == 0) { multiplier = 1; }
+  double PIDangle = error * Kp + (error - lastError) * Kd;
+  lastError = error;
 
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print(middleSonar.ping_cm());
-    display.print(" ");
-    display.print(rightSonar.ping_cm());
-    display.print(" ");
-    display.print(leftSonar.ping_cm());
-    display.println();
-    // display.setCursor(0, 30); 
-    display.print("ang = "); 
-    display.println(int(angle)); 
-    display.print("Kp:"); 
-    display.println(Kp); 
-    display.print("sp = "); 
-    display.println(forwardSpeed); 
-    display.display();
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(middleSonar.ping_cm());
+  display.print(" ");
+  display.print(rightSonar.ping_cm());
+  display.print(" ");
+  display.print(leftSonar.ping_cm());
+  display.println();
+  // display.setCursor(0, 30);
+  display.print("ang = ");
+  display.println(int(PIDangle));
+  display.print("Kp:");
+  display.println(Kp);
+  display.print("Kd:");
+  display.println(Kd);
+  display.print("speed = ");
+  display.println(forwardSpeed);
+  display.display();
 
   if (gameStarted == 1) {
-    int steer_angle = midAngle; 
-    if(angle > 0)
-    {
-      steer_angle = midAngle + min(halfAngleRange, angle); 
-      
+    int steer_angle = midAngle;
+    if (PIDangle > 0) {
+      steer_angle = midAngle + min(halfAngleRange, PIDangle);
+    } else {
+      steer_angle = midAngle - min(halfAngleRange, abs(PIDangle));
     }
-    else {
-      steer_angle = midAngle - min(halfAngleRange, abs(angle)); 
-
-    }
-    steering_servo.write(steer_angle); 
-    //   if (frontDistance != 0 && frontDistance < frontDistanceThreshold) {
-    //     Serial.println("Time to turn!");
-    //     steering_servo.write(leftAngle);
-    //     while (middleSonar.ping_cm() != 0) {
-    //       checkButton();
-    //       delay(30);
-    //       //Serial.println("In the while loop");
-    //     }
-    //     steering_servo.write(midAngle);
-    //   } else if (leftDistance >= rightDistance) {
-    //     error = leftDistance - rightDistance;
-    //     steer_angle = (error / maxError) * halfAngleRange;
-    //     steer_angle = midAngle + steer_angle;
-    //     Serial.print("steer = ");
-    //     Serial.print(steer_angle);
-    //     Serial.println();
-    //   } else if (leftDistance < rightDistance) {
-    //     error = rightDistance - leftDistance;
-    //     steer_angle = (error / maxError) * halfAngleRange;
-    //     steer_angle = midAngle - steer_angle;
-    //     // Serial.println("Calculating right going angle.");
-    //   }
-    //   //erial.println(steer_angle);
-    //   if (steer_angle <= leftAngle and steer_angle >= rightAngle) {
-    //     steering_servo.write(steer_angle);
-    // }
-}
+    steering_servo.write(steer_angle);
+  }
 }
