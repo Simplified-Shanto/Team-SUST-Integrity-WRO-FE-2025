768b1e5c526e13c3886f4f4d3e6a3021d4c5adcb
Nayeem Islam Shanto <islamnayeem386@gmail.com>
2025-06-12 09:06:10 +0600

    Added Picamera v1.3 compatible codes
    1. Added picamera v1.3 streaming code
    2. Created a version of 'ColorPicker'
    code compatible with picamera v1.3

---
+++ Raspberry pi codes/[01] OpenCV codes/[02] Color Picker using piecam v1.3.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2
+from picamera2 import Picamera2
+
+
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (640, 480)})
+picam2.configure(config)
+picam2.start()
+
+
+def empty():
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+path = 'Resources/lambo.png'
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Value Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 255, 179, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
+cv2.createTrackbar("Value Max", "TrackBars", 255, 255, empty)
+
+
+
+while True:
+    img = picam2.capture_array()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max","TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min","TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max","TrackBars")
+    v_min = cv2.getTrackbarPos("Value Min","TrackBars")
+    v_max = cv2.getTrackbarPos("Value Max","TrackBars")
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+
+    #print(h_min, h_max, s_min, s_max, v_min, v_max)
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask = mask)
+
+
+    #imgStack = stackImages(0.5, ([img, imgHSV], [mask, imgResult]))
+    #imgStack = cv2.flip(imgStack, 1)
+
+    cv2.imshow("HSV", img)
+    cv2.waitKey(1)
