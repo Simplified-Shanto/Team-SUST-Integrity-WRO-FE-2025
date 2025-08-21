782e559451a57295cd4f6a4a64ae6e7b6f723dcd
Nayeem Islam Shanto <117001505+Simplified-Shanto@users.noreply.github.com>
2025-08-21 13:14:54 +0600

    ,

--- source code/First_Round/Approach3-CountingLinesWithCameraAlgorithm2/(02) line counter with web cam.py
+++ source code/First_Round/Approach3-CountingLinesWithCameraAlgorithm2/(02) line counter with web cam.py
@@ -114,7 +114,7 @@ while True:
 
     if line_count==12 and SERIAL_READY: 
         message = 'x;' #Commands to stop the car. 
-        time.sleep(3)  # Waiting a bit to reach the center fo the tunnel. 
+        time.sleep(2)  # Waiting a bit to reach the center fo the tunnel. 
         ser.write(message.encode('utf-8'))
         time.sleep(1)
         line_count = -1
