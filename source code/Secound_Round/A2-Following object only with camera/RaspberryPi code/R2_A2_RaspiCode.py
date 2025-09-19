a4971468e5dddf3a4ece42c5ad8aa9d1f56ffd44
Nayeem Islam Shanto <117001505+Simplified-Shanto@users.noreply.github.com>
2025-09-19 21:32:21 +0600

    Update R2_A2_RaspiCode.py

--- source code/Secound_Round/A2-Following object only with camera/RaspberryPi code/R2_A2_RaspiCode.py
+++ source code/Secound_Round/A2-Following object only with camera/RaspberryPi code/R2_A2_RaspiCode.py
@@ -27,7 +27,7 @@ FRAME_CENTER_X = FRAME_WIDTH/2
 
 MIN_OBJECT_AREA = 500  # Minimum contour area to consider an object (adjust as needed)
 MIN_LINE_AREA = 1000
-frontDistance = 35
+frontDistance = 45
 
 if MACHINE == 1 and CAM_TYPE==0: 
     from picamera2 import Picamera2
@@ -235,7 +235,7 @@ blue_line_timer = time.time() * 1000 # Getting the total execution time in milli
 orange_line_timer = blue_line_timer
 orange_line_count = -1
 
-setPoint = 120 # We want the object to be in the center of the frame. 
+setPoint = 0 # We want the object to be in the center of the frame. 
 carStopped = 0 # This scripts assumes that, the car is in motion in the beginning of this script. 
 
 
@@ -479,12 +479,14 @@ while True:
         if obstaclePresent:
                if SERIAL_READY==1: 
                     if distance < frontDistance: 
-                        ser.write(f"R:{distance}".encode('utf-8'))
+                        ser.write(f"Z:{distance};".encode('utf-8'))
+                        if DEVELOPING:
+                            print(f"Serial: Z:{distance};")
                     elif distance > frontDistance: 
                         ser.write(f"E:{error};".encode('utf-8'))
+                        if DEVELOPING:
+                            print(f"Serial: E:{error};")
 
-                    if DEVELOPING:
-                        print(f"Serial: R:{error};")
  
            
 
