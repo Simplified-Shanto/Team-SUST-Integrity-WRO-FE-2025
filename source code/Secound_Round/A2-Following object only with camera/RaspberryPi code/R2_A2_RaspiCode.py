c4f1c1aa2209107d2f939e8f3ffcaddcb6235bfd
Nayeem Islam Shanto <117001505+Simplified-Shanto@users.noreply.github.com>
2025-09-21 20:55:57 +0600

    Update R2_A2_RaspiCode.py

--- source code/Secound_Round/A2-Following object only with camera/RaspberryPi code/R2_A2_RaspiCode.py
+++ source code/Secound_Round/A2-Following object only with camera/RaspberryPi code/R2_A2_RaspiCode.py
@@ -14,7 +14,7 @@ SERIAL_READY = 1 #Whether a serial device is connected or not
 CAM_TYPE = 0 # 0  = Raspicamera, 1  = webcam.
 FOCAL_LENGTH_PX = 335 #Focal length in pixels - 530 for micropack webcam 335 - raspi cam
 BRIGHT_LIGHT = 0 # Bright_light = 1 indicates that we are testing things in bright daylight, bright_light = 0 means that we are testing this thing in night under led lights
-TUNE_HSV = 1 # whether we want to tune the hsv color values for different image elements. 
+TUNE_HSV = 0 # whether we want to tune the hsv color values for different image elements. 
 
 CAMERA_INDEX = 0    # Select which cam will be used  #1 - laptop's camera #0 - micropack webcam
 COM_PORT = 4
@@ -26,7 +26,7 @@ FRAME_HEIGHT = 480
 FRAME_CENTER_X = FRAME_WIDTH/2
 
 MIN_OBJECT_AREA = 400  # Minimum contour area to consider an object (adjust as needed)
-MIN_LINE_AREA = 1000
+MIN_LINE_AREA = 5000
 
 
 if MACHINE == 1 and CAM_TYPE==0: 
@@ -315,7 +315,7 @@ while True:
         if blue_line_count!=-1:  # We'll only do the following processes when blue_line_count is set to zero by pressing the game start button in the vehicle and receiving serial command 'r' from the LLMC. The reason is avoiding early count of the lines by environmental noise before the round has started. 
             for contour_index, contour in enumerate(blue_line_contours): 
                 area = cv2.contourArea(contour)
-                print(f"Blue Area = {area}"); 
+                #print(f"Blue Area = {area}"); 
                 max_blue_line_area = max(max_blue_line_area, area)
                     # if(current_time - blue_line_timer > lineInterval):
                     #     blue_line_count +=1
@@ -325,7 +325,7 @@ while True:
             max_orange_line_area = 0
             for cntour_index, contour in enumerate(orange_line_contours): 
                 area = cv2.contourArea(contour)
-                print(f"Orange Aera = {area}"); 
+                #print(f"Orange Aera = {area}"); 
                 max_orange_line_area = max(max_orange_line_area, area)
      
             if directionSentFlag == 0:
